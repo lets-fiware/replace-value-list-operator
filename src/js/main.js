57cbff4d5710befdb37bbab33982c7408fd347e1
Kazuhito Suda <kazuhito@fisuda.jp>
2019-11-23 15:51:20 +0900

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,76 @@
+/*
+ * replace-value-list
+ * https://github.com/lets-fiware/replace-value-list-operator
+ *
+ * Copyright (c) 2019 Kazuhito Suda
+ * Licensed under the MIT license.
+ */
+
+(function () {
+
+    "use strict";
+
+    var parseInputEndpointData = function parseInputEndpointData(data) {
+        if (typeof data === "string") {
+            try {
+                data = JSON.parse(data);
+            } catch (e) {
+                throw new MashupPlatform.wiring.EndpointTypeError();
+            }
+        }
+
+        if (data != null && !Array.isArray(data)) {
+            throw new MashupPlatform.wiring.EndpointTypeError();
+        }
+
+        return data;
+    };
+
+    var getReplace = function getReplace() {
+        var array = {};
+        var values = MashupPlatform.prefs.get("replace").trim().split(',');
+
+        if (values.length % 2 == 0) {
+            for (var i = 0; i < values.length / 2; i++) {
+                array[values[i]] = values[i + 1];
+            }
+        } else {
+            throw new MashupPlatform.wiring.EndpointValueError();
+        }
+        return array;
+    }
+
+    var pushEvent = function pushEvent(data) {
+        if (MashupPlatform.operator.outputs.output.connected) {
+            MashupPlatform.wiring.pushEvent("output", data);
+        }
+    }
+
+    var replaceList = function replaceList(list) {
+        var replace = getReplace();
+        list = parseInputEndpointData(list);
+
+        if (list != null) {
+            var newList = list.map(function (value) {
+                return replace[value] || value;
+            });
+            pushEvent(newList);
+        } else {
+            if (MashupPlatform.prefs.get("send_nulls")) {
+                pushEvent(list);
+            }
+        }
+    };
+
+    /* TODO
+     * this if is required for testing, but we have to search a cleaner way
+     */
+    if (window.MashupPlatform != null) {
+        MashupPlatform.wiring.registerCallback("input", replaceList);
+    }
+
+    /* test-code */
+    window.replaceList = replaceList;
+    /* end-test-code */
+
+})();
---
+++ tests/js/ReplaceValueListSpec.js
@@ -0,0 +1,93 @@
+/*
+ * replace-value-list
+ * https://github.com/lets-fiware/replace-value-list-operator
+ *
+ * Copyright (c) 2019 Kazuhito Suda
+ * Licensed under the MIT license.
+ */
+
+/* globals MockMP */
+
+(function () {
+
+    "use strict";
+
+    describe("ReplaceValueList", function () {
+
+        beforeAll(function () {
+            window.MashupPlatform = new MockMP({
+                type: 'operator',
+                prefs: {
+                    "send_nulls": true,
+                    "replace": "-9999,missing data"
+                },
+                inputs: ['input'],
+                outputs: ['output']
+            });
+        });
+
+        beforeEach(function () {
+            MashupPlatform.reset();
+            MashupPlatform.operator.outputs.output.connect({simulate: () => {}});
+        });
+
+        it("no replace", function () {
+            replaceList([1, 2, 3]);
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', [1, 2, 3]);
+        });
+
+        it("replace", function () {
+            replaceList([-9999, 2, 3]);
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', ["missing data", 2, 3]);
+        });
+
+        it("allowed to send nulls", function () {
+            MashupPlatform.prefs.set("send_nulls", true);
+
+            replaceList(null);
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', null);
+        });
+
+        it("disallowed to send nulls", function () {
+            MashupPlatform.prefs.set("send_nulls", false);
+
+            replaceList(null);
+
+            expect(MashupPlatform.wiring.pushEvent).not.toHaveBeenCalled();
+        });
+
+        it("throws an Endpoint Value error if illegal data is received", function () {
+            expect(function () {
+                replaceList("{");
+            }).toThrowError(MashupPlatform.wiring.EndpointTypeError);
+        });
+
+        it("throws an Endpoint Value error if illegal data is received", function () {
+            expect(function () {
+                replaceList(123);
+            }).toThrowError(MashupPlatform.wiring.EndpointTypeError);
+        });
+
+        it("throws an Endpoint Value error if replace parameter is error", function () {
+            MashupPlatform.prefs.set("replace", "123,abc,xyz");
+
+            expect(function () {
+                replaceList(5);
+            }).toThrowError(MashupPlatform.wiring.EndpointValueError);
+        });
+
+        it("output endoint is not connected", function () {
+            MashupPlatform.reset();
+            MashupPlatform.prefs.set("replace", "-9999,missing data");
+            MashupPlatform.prefs.set("send_nulls", false);
+
+            replaceList(null);
+
+            expect(MashupPlatform.wiring.pushEvent).not.toHaveBeenCalled();
+        });
+
+    });
+})();
